fb569889908cdfed2d91bcc587029aa18a7f8543
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-18 22:03:20 +0000

    star wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/**
+ * Print all characters of a Star Wars movie based on the Movie ID.
+ *
+ * @param {number} movieId - The Movie ID (e.g., 1 for "A New Hope").
+ */
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
